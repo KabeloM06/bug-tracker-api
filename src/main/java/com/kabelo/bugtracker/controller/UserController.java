ed65b62888ae8487bf219f9fa3f466dae3143d55
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-07 07:55:50 +0200

    Created method to create two dummy users on init. One admin and one regular user

--- src/main/java/com/kabelo/bugtracker/controller/UserController.java
+++ src/main/java/com/kabelo/bugtracker/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kabelo.bugtracker.controller;
 
 import com.kabelo.bugtracker.entity.User;
 import com.kabelo.bugtracker.service.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostConstruct
+    public void initRolesAndUsers(){
+        userService.initRolesAndUser();
+    }
 
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user){
--- src/main/java/com/kabelo/bugtracker/service/UserService.java
+++ src/main/java/com/kabelo/bugtracker/service/UserService.java
@@ -1,9 +1,14 @@
 package com.kabelo.bugtracker.service;
 
+import com.kabelo.bugtracker.dao.RoleDao;
 import com.kabelo.bugtracker.dao.UserDao;
+import com.kabelo.bugtracker.entity.Role;
 import com.kabelo.bugtracker.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
@@ -11,7 +16,42 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private RoleDao roleDao;
+
     public User registerNewUser(User user){
         return userDao.save(user);
+    }
+
+    public void initRolesAndUser(){
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admn role");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default role for new user");
+        roleDao.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        adminUser.setUserName("admin123");
+        adminUser.setUserEmail("admin@test.com");
+        adminUser.setUserPassword("admin@pass1");
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        userDao.save(adminUser);
+
+        User user = new User();
+        user.setUserFirstName("user");
+        user.setUserLastName("user");
+        user.setUserName("user123");
+        user.setUserEmail("user@test.com");
+        user.setUserPassword("user@pass1");
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        userDao.save(user);
     }
 }
